eb928010eea52df1744ab673f7bc0cd5f3b93e43
Takahito Urai <uraitakahito@gmail.com>
2024-10-15 09:27:57 +0000

    Refactor eslint.config.js and package.json to include vitest global variables and update eslint-plugin-vitest to v0.5.4

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,35 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
-    ignores: ['eslint.config.js', '.Trash-*/**/*'],
-    rules: {
-        '@typescript-eslint/no-explicit-any': ['off'],
+const testJestConfig = {
+    files: ['{test,test-jest}/**/*.test.{ts,tsx}'],
+    languageOptions: {
+        globals: {
+            ...globals.jest,
+        },
     },
-});
+};
+
+export default [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['{src,test,test-jest}/**/*.{ts,tsx}'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+        rules: {
+            '@typescript-eslint/array-type': 'error',
+            '@typescript-eslint/consistent-type-imports': 'error',
+            '@typescript-eslint/no-explicit-any': ['off'],
+        },
+    },
+    {
+        ignores: ['.Trash*', 'dist', 'node_modules', '*.js'],
+    },
+    testJestConfig,
+];

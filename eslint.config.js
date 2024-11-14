d196ff269d9f0538e4766907441e5ac4c5764aa3
Takahito Urai <uraitakahito@gmail.com>
2024-11-14 20:10:20 +0900

    Remove @html-eslint dependencies and make ESLint configuration empty

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,5 @@
-import html from '@html-eslint/eslint-plugin';
 export default [
-    // your own configurations.
-    {
-        // recommended configuration included in the plugin
-        ...html.configs['flat/recommended'],
-        files: ['**/*.html'],
-        ignores: ['coverage/**/*'],
-    },
+  {
+    ignores: ["coverage"],
+  },
 ];

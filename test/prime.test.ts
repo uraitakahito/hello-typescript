75b726b4b52d35fb44015eab7c1c458ed7d7fb93
Takahito Urai <uraitakahito@gmail.com>
2024-12-29 08:38:35 +0900

    Addressing ESLint issues

--- test/prime.test.ts
+++ test/prime.test.ts
@@ -2,22 +2,22 @@ import primenumber from '../src/prime';
 
 describe('primenumber', () => {
   it('should return false when n is less than 1', () => {
-    const result = primenumber(0);
-    expect(result).toBe(false);
+    const isprime = primenumber(0);
+    expect(isprime).toBe(false);
   });
 
   it('should return false when n is not a prime number', () => {
-    const result = primenumber(4);
-    expect(result).toBe(false);
+    const isprime = primenumber(4);
+    expect(isprime).toBe(false);
   });
 
   it('should return true when n is a prime number', () => {
-    const result = primenumber(7);
-    expect(result).toBe(true);
+    const isprime = primenumber(7);
+    expect(isprime).toBe(true);
   });
 
   it('should return true when n is a large prime number', () => {
-    const result = primenumber(997);
-    expect(result).toBe(true);
+    const isprime = primenumber(997);
+    expect(isprime).toBe(true);
   });
 });
